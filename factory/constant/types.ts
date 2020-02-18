ac29849ee5871ec0a9b18621f6a69db0f2b7c30b
kim jihyun <>
2020-02-19 08:43:29 +0900

    fix

--- factory/constant/types.ts
+++
@@ -1,7 +0,0 @@
-export const TYPES = {
-    SpeakService: Symbol('SpeakService'),
-    FactoryService: Symbol('FactoryService'),
-    ConnectService: Symbol('ConnectService'),
-    ProviderService: Symbol('ProviderService'),
-    Robot: Symbol('Robot'),
-}
--- factory/index.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { container } from './inversify.config';
-import { TYPES } from './constant/types';
-import { IRobot } from './robot';
-
-const robot = container.get<IRobot>(TYPES.Robot);
-
-robot.start('yogae').then();
